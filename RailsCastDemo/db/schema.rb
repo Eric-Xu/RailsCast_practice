77608d504627da57e0434ab00386c17942ab2eee
Eric Xu <e.xulander@gmail.com>
2013-04-21 22:46:00 -0700

    add task model

--- RailsCastDemo/db/schema.rb
+++ RailsCastDemo/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130422053030) do
+ActiveRecord::Schema.define(:version => 20130422054452) do
+
+  create_table "tasks", :force => true do |t|
+    t.string   "name"
+    t.integer  "project_id"
+    t.boolean  "complete"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
