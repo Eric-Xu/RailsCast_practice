26f0cf262c6a59cc56a865d0710f312d2486c6bb
Eric Xu <e.xulander@gmail.com>
2013-04-21 22:50:15 -0700

    enable dynamic find_by methods from ep2

---
+++ RailsCastDemo/app/controllers/tasks_controller.rb
@@ -0,0 +1,9 @@
+class TasksController < ApplicationController
+	def incomplete
+		@tasks = Task.find_all_by_complete(false)
+	end
+
+	def last_incomplete
+		@task = Task.find_by_complete(false, :order => 'created_at DESC')
+	end
+end
