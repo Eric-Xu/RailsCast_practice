00d8186961c1dc63bceeaacb3f2e750b844424af
Eric Xu <e.xulander@gmail.com>
2013-04-21 22:17:14 -0700

    first commit

---
+++ RailsCastDemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCastDemo::Application.config.secret_token = '56e49d4765115e4529d4c7619da3ee8857bf0d1d3ec5743cd4a418000695314f90f40d83c7abb1871ca171e3125300e94058d4161f8745072644d1aabb7c8517'
