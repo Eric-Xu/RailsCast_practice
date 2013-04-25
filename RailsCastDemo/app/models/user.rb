a7d8f58f13547e846120b23e98301c750a68fa3f
Eric Xu <e.xulander@gmail.com>
2013-04-25 14:59:47 -0700

    refactor full name method in user model from ep11

--- RailsCastDemo/app/models/user.rb
+++ RailsCastDemo/app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :middle_initial, :password, :password_confirmation
 
   def full_name
-  	if middle_initial.blank?
-  		first_name + " " + last_name
-  	else
-  		first_name + " #{middle_initial}. " + last_name
-  	end
+  	[first_name, middle_initial_with_full_stop, last_name].compact.join(" ")
+  end
+
+  def middle_initial_with_full_stop
+  	"#{middle_initial}." unless middle_initial.blank?
   end
 end
