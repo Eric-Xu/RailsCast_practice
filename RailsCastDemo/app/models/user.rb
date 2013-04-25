f11d4b9fd98b2a2c3cbb66d3948a6c9aa7c3a459
Eric Xu <e.xulander@gmail.com>
2013-04-25 00:12:33 -0700

    refactor full name to user model from ep10

--- RailsCastDemo/app/models/user.rb
+++ RailsCastDemo/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :middle_initial, :password, :password_confirmation
+
+  def full_name
+  	if middle_initial.blank?
+  		first_name + " " + last_name
+  	else
+  		first_name + " #{middle_initial}. " + last_name
+  	end
+  end
 end
