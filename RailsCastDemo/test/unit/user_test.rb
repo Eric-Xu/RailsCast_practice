b633ba60a0f2f196b8fc4e3bc1ca413edba45844
Eric Xu <e.xulander@gmail.com>
2013-04-25 14:53:54 -0700

    write user unit tests

--- RailsCastDemo/test/unit/user_test.rb
+++ RailsCastDemo/test/unit/user_test.rb
@@ -1,41 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  fixtures :users
 
-  def test_full_name_without_middle_initial
-  	user = User.new(:first_name => 'John', :last_name => 'Doe')
-  	assert_equal 'John Doe', user.full_name
+  test "full name with middle initial" do
+    user = User.new(first_name: "John", middle_initial: "H", last_name: "Smith")
+    assert_equal 'John H. Smith', user.full_name
   end
 
-  def test_full_name_with_middle_initial
-  	user = User.new(:first_name => 'John', :middle_initial => 'H', :last_name => 'Doe')
-  	assert_equal 'John H. Doe', user.full_name
+  test "full name without middle initial" do
+    user = User.new(first_name: "John", last_name: "Smith")
+    assert_equal 'John Smith', user.full_name
   end
 
-  def test_full_name_with_blank_middle_initial
-  	user = User.new(:first_name => 'John', :middle_initial => '', :last_name => 'Doe')
-  	assert_equal 'John Doe', user.full_name
+  test "full name with blank middle initial" do
+    user = User.new(first_name: "John", middle_initial: "", last_name: "Smith")
+    assert_equal 'John Smith', user.full_name
   end
 end
-
-# require File.dirname(__FILE__) + '/../test_helper'
-
-# class UserTest < Test::Unit::TestCase
-#   fixtures :users
-
-#   def test_full_name_without_middle_initial
-#   	user = User.new(:first_name => 'John', :last_name => 'Doe')
-#   	assert_equal 'John Doe', user.full_name
-#   end
-
-#   def test_full_name_with_middle_initial
-#   	user = User.new(:first_name => 'John', :middle_initial => 'H', :last_name => 'Doe')
-#   	assert_equal 'John H. Doe', user.full_name
-#   end
-
-#   def test_full_name_with_blank_middle_initial
-#   	user = User.new(:first_name => 'John', :middle_initial => '', :last_name => 'Doe')
-#   	assert_equal 'John Doe', user.full_name
-#   end
-# end
