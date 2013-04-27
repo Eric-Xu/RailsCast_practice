6a31c9589cbb57d4800064be6a149f7323192e5f
Eric Xu <e.xulander@gmail.com>
2013-04-26 23:33:08 -0700

    refactor unit test from ep12

--- RailsCastDemo/test/unit/user_test.rb
+++ RailsCastDemo/test/unit/user_test.rb
@@ -2,18 +2,13 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "full name with middle initial" do
-    user = User.new(first_name: "John", middle_initial: "H", last_name: "Smith")
-    assert_equal 'John H. Smith', user.full_name
+  test "full name" do
+    assert_equal 'John H. Smith', full_name_test('John', 'H', 'Smith'), 'with middle initial'
+    assert_equal 'John Smith', full_name_test('John', nil, 'Smith'), 'nil middle initial'
+    assert_equal 'John Smith', full_name_test('John', '', 'Smith'), 'blank middle initial'
   end
 
-  test "full name without middle initial" do
-    user = User.new(first_name: "John", last_name: "Smith")
-    assert_equal 'John Smith', user.full_name
-  end
-
-  test "full name with blank middle initial" do
-    user = User.new(first_name: "John", middle_initial: "", last_name: "Smith")
-    assert_equal 'John Smith', user.full_name
+  def full_name_test(first, middle, last)
+    User.new(first_name: first, middle_initial: middle, last_name: last).full_name
   end
 end
