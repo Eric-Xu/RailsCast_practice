0df2fd9124974810113f8f08a4b6035ba27cbaf2
Eric Xu <e.xulander@gmail.com>
2013-04-27 23:56:46 -0700

    run ActiveRecord calculation methods on models from ep14

--- RailsCastDemo/app/models/task.rb
+++ RailsCastDemo/app/models/task.rb
@@ -8,4 +8,20 @@ class Task < ActiveRecord::Base
 	  	find_all_by_complete(false)
 	  end
   end
+
+	# Episode 15 Fun with Find Conditions
+		# Task.sum(:priority)
+	  # SELECT SUM("tasks"."priority") AS sum_id FROM "tasks"
+
+	 	# Task.sum(:priority, :conditions =>["complete = ?", false])
+	 	# SELECT SUM("tasks"."priority") AS sum_id FROM "tasks" WHERE (complete = 'f')
+
+	 	# Task.maximum(:priority)
+	 	# SELECT MAX("tasks"."priority") AS max_id FROM "tasks"
+
+	 	# Task.average(:priority)
+	 	# SELECT AVG("tasks"."priority") AS avg_id FROM "tasks"
+
+	 	# Project.first.tasks.sum(:priority)
+	 	# SELECT SUM("tasks"."priority") AS sum_id FROM "tasks" WHERE "tasks"."project_id" = 1
 end
