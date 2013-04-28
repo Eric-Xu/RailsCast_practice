82d44d3ed5ba66bd875907d37ff56025c3ffe761
Eric Xu <e.xulander@gmail.com>
2013-04-27 23:58:08 -0700

    fun with find conditions from ep15

--- RailsCastDemo/app/controllers/tasks_controller.rb
+++ RailsCastDemo/app/controllers/tasks_controller.rb
@@ -10,4 +10,13 @@ class TasksController < ApplicationController
 	def last_incomplete
 		@task = Task.find_by_complete(false, :order => 'created_at DESC')
 	end
+	# Episode 15 Fun with Find Conditions
+		# Task.find(:all, :conditions => ["complete = ? AND priority = ?", false, 2])
+		# SELECT "tasks".* FROM "tasks" WHERE (complete = 'f' AND priority = 2)
+
+		# Task.find(:all, :conditions => { complete: false, priority: [1,3] })
+		# SELECT "tasks".* FROM "tasks" WHERE "tasks"."complete" = 'f' AND "tasks"."priority" IN (1, 3)
+
+		# Task.find_all_by_priority(1..3)
+		# SELECT "tasks".* FROM "tasks" WHERE ("tasks"."priority" BETWEEN 1 AND 3)
 end
