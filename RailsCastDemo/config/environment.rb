e17400a19eae7a19c11aa0c71c2bf04c39d40e9f
Eric Xu <e.xulander@gmail.com>
2013-05-21 22:37:36 -0700

    add a custom date format to the to_s method from ep31

--- RailsCastDemo/config/environment.rb
+++ RailsCastDemo/config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RailsCastDemo::Application.initialize!
+
+Time::DATE_FORMATS[:due_date] = "%-m/%-d/%Y at %I:%M %p"
