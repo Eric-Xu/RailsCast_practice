8dd69a0be18056cb461bd7ed0c516c052992c47e
Eric Xu <e.xulander@gmail.com>
2013-05-26 15:31:21 -0700

    add description column to projects model

--- RailsCastDemo/db/schema.rb
+++ RailsCastDemo/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130511223858) do
+ActiveRecord::Schema.define(:version => 20130526222949) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
     t.integer  "tasks_count", :default => 0
+    t.text     "description"
   end
 
   create_table "tasks", :force => true do |t|
