d3bef02fa10fdbb257a5a61656bbb38c897b8dee
Eric Xu <e.xulander@gmail.com>
2013-05-30 21:47:48 -0700

    create user destroy action and add delete links on user show page

--- RailsCastDemo/app/controllers/users_controller.rb
+++ RailsCastDemo/app/controllers/users_controller.rb
@@ -19,4 +19,11 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:notice] = "User successfully deleted"
+		redirect_to users_path
+	end
 end
