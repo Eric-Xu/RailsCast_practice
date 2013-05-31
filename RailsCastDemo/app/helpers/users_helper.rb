9c91fa0284a93c1652ebf25817b5291939120ed1
Eric Xu <e.xulander@gmail.com>
2013-05-30 23:10:56 -0700

    create a custom helper method to display delete link on user show page; ep40

--- RailsCastDemo/app/helpers/users_helper.rb
+++ RailsCastDemo/app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+	# Episode 40
+	# Move this to the Application Helper in order to share with other model view templates
+	def admin_area(&block) # being called in the user show template
+		# if current_user.admin?
+		if true
+			concat content_tag(:span, capture(&block), class: 'admin')
+		end
+	end
 end
