e98e09722304fc950b91e8bd943db39165b72c81
Eric Xu <e.xulander@gmail.com>
2013-06-01 22:16:17 -0700

    add basic presence validation and error styling to user model

--- RailsCastDemo/app/models/user.rb
+++ RailsCastDemo/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :middle_initial, :password, :password_confirmation
 
+  validates_presence_of :first_name, :last_name
+  validates_presence_of :password, :password_confirmation
+
   def full_name
   	[first_name, middle_initial_with_full_stop, last_name].compact.join(" ")
   end
